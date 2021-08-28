7ceaab16e178d3c3db3c053b020699e5b28b104e
umurabali <umurabali@gmail.com>
2021-08-28 11:14:37 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React, {useEffect, useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [notToDoItems, setNotToDoItems] = useState([])
+
   return (
     <div className="App">
       <header className="App-header">
